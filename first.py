a10b03f220b0ad600a4fe00ac8bfed3f01a10e38
Huzaifa-Saleem98 <huzaifasaleem257@gmail.com>
2025-04-11 20:10:22 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,29 @@
+print("Huzaifa saleem")
+
+print("Q1")
+sum =(5+5+5)
+sub =(10-2-5)
+product =(2*3*2)
+average =(30+20+20/3)
+print(sum)
+print(sub)
+print(product)
+print(average)
+
+print("Q2")
+area1 =(0.5*5*4)
+area2 =(4*4)
+area3 =(3.14*6*2) #3.14 is the value of Pi 
+area4 =(9*2)
+print(area1)
+print(area2)
+print(area3)
+print(area4)
+
+print("Q3")
+x =(10)
+y =(12)
+x,y = y,x
+print("after swaping, the value of x will equals to value of y")
+print(x)
+print(y)      
